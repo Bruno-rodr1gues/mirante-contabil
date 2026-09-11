d9b3e3857d1789e71a83295d847e819ae0286269
Bruno Rodrigues <brunno.rodrigues0706@gmail.com>
2026-09-10 22:16:16 -0300

    [FRONT-END] - Criação do dashboard apresentativo

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardTitleGroup } from '@angular/material/card';
 
 @Component({
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardTitleGroup, MatCardTitle, MatCardContent],
   selector: 'app-dashboard',
   styleUrl: './dashboard.component.css',
   templateUrl: './dashboard.component.html',
